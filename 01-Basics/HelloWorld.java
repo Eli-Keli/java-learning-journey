a0acd696af041aa1b1bf25f4927dcc21f3cc01e0
Eli-Keli <muthokaelikeli@gmail.com>
2025-03-22 21:40:37 +0300

    Added HelloWorld.java - First Java program

---
+++ 01-Basics/HelloWorld.java
@@ -0,0 +1,9 @@
+
+// Class Definition: Every Java program must have a class.
+public class HelloWorld {
+
+    // Main Method: The entry point where execution starts.
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Print the message to the console.
+    }
+}
